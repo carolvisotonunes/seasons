2e770e9451085992ed38008c6e3442cb980184ce
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-04-29 18:55:56 +1000

    update state properties

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import ReactDOM from 'react-dom';
 class App extends React.Component{
    constructor(props){
       super(props);
+
+      //THIS IS THE ONLY TIME we do direct assignment to this.state
       this.state= {lat: null};
    }
 
    render(){
       window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
+        position =>{
+          //we called setstate!!!
+          this.setState({lat: position.coords.latitude});
+        },
         err => console.log(err)
       );
-      return <div>Latitude: </div>;
+      return <div>Latitude: {this.state.lat} </div>;
    }
 }
 
