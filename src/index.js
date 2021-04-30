adcda80bf1d91e9e65cecea6bcb319406244f5fe
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-04-30 17:58:56 +1000

    add loading spinner

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-class App extends React.Component{
-
-   state = { lat: null, errorMessage: "" };
-
-   componentDidMount(){
-       window.navigator.geolocation.getCurrentPosition(
-              position => this.setState({lat: position.coords.latitude}),
-              err => this.setState({ errorMessage: err.message })
-       );
-   }
-
-   // React says we have to define render!!
-     render() {
-        if(this.state.errorMessage && !this.state.lat){
-          return <div>Error: {this.state.errorMessage}</div>;
-        }
-        if(!this.state.errorMessage && this.state.lat){
-          return <SeasonDisplay lat = {this.state.lat}/>;
-        }
-        return <div>Loading.....</div>;
-     }
+if (module.hot) {
+  module.hot.accept();
 }
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner />;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
